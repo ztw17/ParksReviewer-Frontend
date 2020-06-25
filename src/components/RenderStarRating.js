39cf0d89068655bc59709bb6bf84d9a36241d5e0
Zach Weber <weberzt@gmail.com>
2020-06-25 11:39:22 -0500

    refactored renderstarrating component to remove unneeded code

--- src/components/RenderStarRating.js
+++ src/components/RenderStarRating.js
@@ -3,17 +3,13 @@ import Rating from '@material-ui/lab/Rating';
 import Box from '@material-ui/core/Box';
 
 export default function RenderStarRating(props) {
-  const [value, setValue] = React.useState(0);
 
   return (
     <div>
-      <Box align="left" component="fieldset" mb={3} borderColor="transparent">
+      <Box align="left" mb={1} borderColor="transparent">
         <Rating
           value={props.reviewInfo.rating}
           name="rating"
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
           readOnly={props.readOnly}
         />
       </Box>
